1e758e896df4afe5aa5d73313fe4b0dba7758f4f
Alex_Kelly <afkelly14@Gmail.com>
2020-07-05 15:59:39 -0400

    project complete

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 url = "https://rickandmortyapi.com/api/character/";
 
-popUp = document.querySelector(".information");
+let popUp = document.querySelector(".information");
 
-allcharacters = document.querySelectorAll(".characters");
+let allcharacters = document.querySelectorAll(".characters");
 
 let img = document.querySelectorAll("img");
+
+let closeScreen = document.querySelector(".close");
 
 fetch(url)
   .then((res) => res.json())
@@ -24,13 +26,20 @@ fetch(url)
       let location = res.results[i].location.name;
 
       img[i].addEventListener("click", openPopUp);
+      closeScreen.addEventListener("click", closePopUp);
 
       function openPopUp(e) {
         e.preventDefault();
         popUp.innerText = `Name : ${name} | Status : ${status} | Species : ${species} | Type : ${type} | Gender : ${gender} | Location : ${location} | Image :
         ${img[i]}`;
 
-        popUp.style.opacity = 1;
+        popUp.style.opacity = 0.75;
+        closeScreen.style.opacity = 1;
+      }
+
+      function closePopUp(e) {
+        popUp.style.opacity = 0;
+        closeScreen.style.opacity = 0;
       }
     }
   });
